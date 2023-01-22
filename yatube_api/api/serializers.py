f692516f39d31519ffd1d7e3888f90bbf0ea4eba
Александр Коваленко <amater95@yandex.ru>
2023-01-22 13:37:34 +0300

    проверка pip

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,72 @@
+import base64
+
+from django.core.files.base import ContentFile
+from posts.models import Comment, Follow, Group, Post, User
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
 
-from posts.models import Comment, Post
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith("data:image"):
+            format, imgstr = data.split(";base64,")
+            ext = format.split("/")[-1]
+            data = ContentFile(base64.b64decode(imgstr), name="temp." + ext)
+        return super().to_internal_value(data)
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field="username"
+    )
+    image = Base64ImageField(required=False, allow_null=True)
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = "__all__"
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = "__all__"
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        read_only=True, slug_field="username"
+    )
+    post = serializers.SlugRelatedField(read_only=True, slug_field="id")
+
+    class Meta:
+        model = Comment
+        fields = "__all__"
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field="username",
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault(),
+    )
+    following = serializers.SlugRelatedField(
+        slug_field="username", queryset=User.objects.all()
     )
 
     class Meta:
-        fields = '__all__'
-        model = Comment
+        model = Follow
+        fields = "__all__"
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=("user", "following"),
+                message="Вы уже подписаны на этого пользователя",
+            )
+        ]
+
+    def validate(self, data):
+        if data["following"] == data["user"]:
+            raise serializers.ValidationError(
+                "Нельзя подписаться на самого себя"
+            )
+        return data
